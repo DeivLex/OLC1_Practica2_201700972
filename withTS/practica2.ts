03e1c782e90eb00914319ca1f856888ef8a237fe
DeivLex <peloliso.98@gmail.com>
2020-04-06 19:13:41 -0600

    Parte Grafica

--- withTS/practica2.ts
+++ withTS/practica2.ts
@@ -1,30 +1,62 @@
-class prueba{
-    mostrarts(): void {
-        var h1:HTMLElement = document.getElementById("h1ts") as HTMLElement;
-        var h2:HTMLElement = document.getElementById("h2ts") as HTMLElement;
-
-        h1.innerText = "Nombre: nombre del alumno con ts";
-        h1.innerHTML = "Nombre: nombre del alumno con ts";
-        h2.innerText = "Carné: carné del alumno con ts";
+//Crear Tabla
+// Obtener la referencia del elemento body
+var body = document.getElementsByTagName("body")[0];
+// Crea un elemento <table> y un elemento <tbody>
+var tabla   = document.createElement("table");
+var tblBody = document.createElement("tbody");
+                       
+function genera_tabla() {   
+    tblBody.innerHTML = "";
+    var hileraP = document.createElement("tr");
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Nombre"));
+    hileraP.appendChild(celdaP);
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Tipo"));
+    hileraP.appendChild(celdaP);
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Linea"));
+    hileraP.appendChild(celdaP);
+    tblBody.appendChild(hileraP);
+    // Crea las celdas
+    for (var i = 0; i < 2; i++) {
+      // Crea las hileras de la tabla
+      var hilera = document.createElement("tr");
+   
+      for (var j = 0; j < 3; j++) {
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        var celda = document.createElement("td");
+        var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+        celda.appendChild(textoCelda);
+        hilera.appendChild(celda);
+      }
+   
+      // agrega la hilera al final de la tabla (al final del elemento tblbody)
+      tblBody.appendChild(hilera);
     }
+   
+    // posiciona el <tbody> debajo del elemento <table>
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+  }
 
-
-    mostrar2():void{
-        var h1:HTMLElement = document.getElementById("h1js") as HTMLElement;
-        var h2:HTMLElement = document.getElementById("h2js") as HTMLElement;
-
-        h1.innerText = "Nombre: nombre del alumno con js";
-        h1.innerHTML = "Nombre: nombre del alumno con js";
-        h2.innerText = "Carné: carné del alumno con js";
-
-    }
-
-    main(){
-        this.mostrarts();
-        this.mostrar2();
-
-    }
+  function saveTextAsFile()  
+{   
+    var textToWrite = (document.getElementById('editor') as HTMLInputElement).value;
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = "myNewFile.er";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "My Hidden Link";
+    window.URL = window.URL || window.webkitURL;
+          
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
 }
-
-let p = new prueba();
-p.main();
--- withTS/src/practica2.js
+++ withTS/src/practica2.js
@@ -1,26 +1,56 @@
 "use strict";
-var prueba = /** @class */ (function () {
-    function prueba() {
+//Crear Tabla
+// Obtener la referencia del elemento body
+var body = document.getElementsByTagName("body")[0];
+// Crea un elemento <table> y un elemento <tbody>
+var tabla = document.createElement("table");
+var tblBody = document.createElement("tbody");
+function genera_tabla() {
+    tblBody.innerHTML = "";
+    var hileraP = document.createElement("tr");
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Nombre"));
+    hileraP.appendChild(celdaP);
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Tipo"));
+    hileraP.appendChild(celdaP);
+    var celdaP = document.createElement("td");
+    celdaP.appendChild(document.createTextNode("Linea"));
+    hileraP.appendChild(celdaP);
+    tblBody.appendChild(hileraP);
+    // Crea las celdas
+    for (var i = 0; i < 2; i++) {
+        // Crea las hileras de la tabla
+        var hilera = document.createElement("tr");
+        for (var j = 0; j < 3; j++) {
+            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+            // texto sea el contenido de <td>, ubica el elemento <td> al final
+            // de la hilera de la tabla
+            var celda = document.createElement("td");
+            var textoCelda = document.createTextNode("celda en la hilera " + i + ", columna " + j);
+            celda.appendChild(textoCelda);
+            hilera.appendChild(celda);
+        }
+        // agrega la hilera al final de la tabla (al final del elemento tblbody)
+        tblBody.appendChild(hilera);
     }
-    prueba.prototype.mostrarts = function () {
-        var h1 = document.getElementById("h1ts");
-        var h2 = document.getElementById("h2ts");
-        h1.innerText = "Nombre: nombre del alumno con ts";
-        h1.innerHTML = "Nombre: nombre del alumno con ts";
-        h2.innerText = "Carné: carné del alumno con ts";
-    };
-    prueba.prototype.mostrar2 = function () {
-        var h1 = document.getElementById("h1js");
-        var h2 = document.getElementById("h2js");
-        h1.innerText = "Nombre: nombre del alumno con js";
-        h1.innerHTML = "Nombre: nombre del alumno con js";
-        h2.innerText = "Carné: carné del alumno con js";
-    };
-    prueba.prototype.main = function () {
-        this.mostrarts();
-        this.mostrar2();
-    };
-    return prueba;
-}());
-var p = new prueba();
-p.main();
+    // posiciona el <tbody> debajo del elemento <table>
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+}
+function saveTextAsFile() {
+    var textToWrite = document.getElementById('editor').value;
+    var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+    var fileNameToSaveAs = "myNewFile.er";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "My Hidden Link";
+    window.URL = window.URL || window.webkitURL;
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
